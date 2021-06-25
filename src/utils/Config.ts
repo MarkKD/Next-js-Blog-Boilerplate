ab9a751e2fdb678b654e0ea047e4952dd174749a
Mark Keenan <markk@deeson.co.uk>
2021-06-25 16:34:06 +0100

    test change

--- src/utils/Config.ts
+++ src/utils/Config.ts
@@ -1,6 +1,6 @@
 export const Config = {
   site_name: 'Starter',
-  title: 'Next.js Boilerplate',
+  title: 'Build change',
   description: 'Starter code for your Next.js blog Boilerplate with Tailwind CSS',
   url: 'https://example.com',
   locale: 'en',
